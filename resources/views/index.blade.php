6014a8a3afa16bab1c6ed8f304c5e03724956cd8
Daeyeol Ryu <dy@yoobato.com>
2015-07-19 03:37:55 +0800

    스타일 일부 변경 // 검색 Input 박스 change 이벤트 발생시 jQuery 연동

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,14 @@
     <div class="jumbotron">
         <h3>어떤 단어가 궁금하신가요?</h3>
         <div class="input-group input-group-lg">
-            <span class="input-group-addon" id="sizing-addon1">@</span>
-            <input type="text" id="query" class="form-control" placeholder="검색어를 입력해주세요" aria-describedby="sizing-addon1">
+            <span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-console"></i></span>
+            <input type="search" id="word_search" class="form-control" placeholder="검색어를 입력해주세요" aria-describedby="sizing-addon1" autofocus="autofocus" autocomplete="off">
         </div>
     </div>
 
     <div class="list-group">
         <a href="#" class="list-group-item disabled">
-            검색결과 (n건)
+            <i class="glyphicon glyphicon-search"></i> 검색결과 (n건)
         </a>
         <a href="#" class="list-group-item">linked list</a>
         <a href="#" class="list-group-item">delegate</a>
@@ -19,7 +19,7 @@
 
     <div class="panel panel-warning">
         <div class="panel-heading">
-            <h3 class="panel-title">검색 결과가 없습니다.</h3>
+            <h3 class="panel-title"><i class="glyphicon glyphicon-alert"></i> 검색 결과가 없습니다.</h3>
         </div>
         <div class="panel-body">
             <a href="#" class="lead">단어를 추가해주시겠어요?</a>
@@ -30,8 +30,25 @@
 @section("script")
     <script>
         $(function() {
-            $("#query").change(function () {
-                // TODO: 검색
+            $("#word_search").each(function() {
+                var element = $(this);
+
+                // Save current value of element
+                element.data("oldVal", element.val());
+
+                // Look for changes in the value
+                element.bind("propertychange change click keyup input past", function(event) {
+                    // If value has changed...
+                    if (element.data("oldVal") != element.val()) {
+                        // Updated stored value
+                        element.data("oldVal", element.val());
+
+                        // Change URL path
+                        window.history.pushState("", "", "/?q=" + $(this).val());
+
+                        // TODO: 검색
+                    }
+                });
             });
         });
     </script>
